fc898b0e12d3b0616a279f9be516899e813908af
Mosaab <aborkiamosaab@gmail.com>
2021-01-22 10:41:17 +0200

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'))
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port,function(){
     console.log("listen")
